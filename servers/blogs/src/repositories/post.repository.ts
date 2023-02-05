aa525ab626bf45341eea343606bd6ac7018ee7d7
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-02-06 00:57:10 +0200

    Encapsulate Timeline QUERY Builder Logic

--- servers/blogs/src/repositories/post.repository.ts
+++ servers/blogs/src/repositories/post.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { Post } from '../entities/post.entity';
 import { CreatePostDto, PostResponse } from '../dtos/post.dto';
 import { PageOptionsDto } from '../dtos/pagination/page.option.dto';
@@ -22,23 +22,7 @@ export class PostRepository {
     pageOptionDto: PageOptionsDto,
     user: User,
   ): Promise<PostResponse> {
-    const queryBuilder = this.postRepository.createQueryBuilder('post');
-
-    queryBuilder
-      .where('post.segmentType = :segmentType', { segmentType: user.role })
-      .orWhere((qb) => {
-        const subQuery = qb
-          .subQuery()
-          .select('user_to_post.postId')
-          .from(UserToPost, 'user_to_post')
-          .where('user_to_post.userId = :userId')
-          .getQuery();
-        return 'post.id IN ' + subQuery;
-      })
-      .setParameter('userId', user.id)
-      .orderBy('post.created_at', pageOptionDto.order)
-      .skip(pageOptionDto.skip)
-      .take(pageOptionDto.take);
+    const queryBuilder = this.timelineQuery(user, pageOptionDto);
 
     const itemCount = await queryBuilder.getCount();
     const { entities } = await queryBuilder.getRawAndEntities();
@@ -72,5 +56,30 @@ export class PostRepository {
     });
 
     return new PostResponse(entities, pageMetaDto);
+  }
+
+  timelineQuery(
+    user: User,
+    pageOptionDto: PageOptionsDto,
+  ): SelectQueryBuilder<Post> {
+    const queryBuilder = this.postRepository.createQueryBuilder('post');
+
+    queryBuilder
+      .where('post.segmentType = :segmentType', { segmentType: user.role })
+      .orWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select('user_to_post.postId')
+          .from(UserToPost, 'user_to_post')
+          .where('user_to_post.userId = :userId')
+          .getQuery();
+        return 'post.id IN ' + subQuery;
+      })
+      .setParameter('userId', user.id)
+      .orderBy('post.created_at', pageOptionDto.order)
+      .skip(pageOptionDto.skip)
+      .take(pageOptionDto.take);
+
+    return queryBuilder;
   }
 }
