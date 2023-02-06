692f7fbc60f3c55491b9838efa68ebc09b04b238
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-02-06 20:10:28 +0200

    Adding Users Table

--- client/src/components/Button/Standard/index.tsx
+++ client/src/components/Button/Standard/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
-export default function UserButtons() {
+export default function Standard() {
   return (
     <React.Fragment>
       <Button variant='outlined' size='small'>
-        Delete
+        Create
       </Button>
     </React.Fragment>
   );
--- client/src/container/User/index.tsx
+++ client/src/container/User/index.tsx
@@ -1,41 +1,71 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import FeaturedPost from '../Post';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ActionButtons from '../../components/Button/ActionButtons';
+import Standard from '../../components/Button/Standard';
 
-const featuredPosts = [
-  {
-    title: 'user post',
-    date: 'Nov 12',
-    description: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
-    imageLabel: 'Image Text',
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
-  {
-    title: 'user title',
-    date: 'Nov 11',
-    description: 'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
-    imageLabel: 'Image Text',
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
   },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(id: number, name: string) {
+  return { id, name };
+}
+
+const rows = [
+  createData(1, 'Frozen yoghurt'),
+  createData(2, 'Ice cream sandwich'),
+  createData(3, 'Eclair'),
+  createData(4, 'Cupcake'),
+  createData(5, 'Gingerbread'),
 ];
 
-const theme = createTheme();
-
-export default function Blog() {
+export default function CustomizedTables() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth='lg'>
-        <main>
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
-        </main>
-      </Container>
-    </ThemeProvider>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align='right'>Actions</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component='th' scope='row'>
+                {row.id}
+              </StyledTableCell>
+              <StyledTableCell>{row.name}</StyledTableCell>
+              <StyledTableCell align='right'>
+                <ActionButtons />
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
